cdcf9d20ee3003b1fff39994408c6eeb0cbffa52
Faithgg <fglindayi@gmail.com>
2025-01-14 16:57:56 +0100

    docker

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ COPY . .
 
 RUN python -m venv myenv
 RUN . myenv/bin/activate
-RUN python app.py
+RUN pip install --upgrade pip
+RUN pip3 install -r requirements.txt
+RUN python3 app.py
--- db/saveVoice.py
+++ db/saveVoice.py
@@ -4,5 +4,6 @@ conn = engine.connect()
 req1 = conn.execute(text(f"CREATE TABLE IF NOT EXISTS voice_ai (id INT AUTO_INCREMENT PRIMARY KEY, text VARCHAR(255) NOT NULL, filename VARCHAR(255) NOT NULL)")) 
 
 def rendeur_de_requetes (prompt :str, filename) :
-    req2 = conn.execute(text(f"INSERT INTO voice_ai (text, filename) VALUES (\"{prompt.replace('\"','_').replace('\'','_')[0:250]}\", '{filename}')")) 
+    cleaned_prompt = prompt.replace('"', '_').replace("'", '_')[:250]
+    req2 = conn.execute(text(f'INSERT INTO voice_ai (text, filename) VALUES ("{cleaned_prompt}", "{filename}")'))
     return [req2]
